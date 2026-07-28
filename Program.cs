b110b941c15c6918fce5e0bf2b11482f272f5004
brennoln <brennolopesnobrega@gmail.com>
2026-07-28 16:04:58 -0300

    feat: adicionei um timer para o usuario não receber emails sucessivos, anti spam

--- Program.cs
+++ Program.cs
@@ -8,13 +8,47 @@ public class Program
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .Build();
         decimal valorAtual = 0m;
+        decimal valMax = 0m;
+        decimal valMin = 0m;
         if(args.Length == 3)
         {
             
 
             AtivoB3 ativo= new AtivoB3();
-            valorAtual = await ativo.ValorAtual(args[0]);
-            Console.WriteLine(valorAtual);
+            valMax = decimal.Parse(args[1]);
+            valMin = decimal.Parse(args[2]);
+            int timer = 1000;
+            int delay = 5;
+            int contador = delay;
+
+            while (true)
+            {
+                valorAtual = await ativo.ValorAtual(args[0]);
+                Console.WriteLine(valorAtual);
+                contador ++;
+                if(valorAtual > valMax)
+                {   
+                    if(contador >= delay)
+                    {
+                        Console.WriteLine("valor acima , venda");
+                        contador = 0;
+                    }
+            
+                }else if(valorAtual < valMin)
+                {   
+                    if(contador >= delay)
+                    {
+                        Console.WriteLine("valor abaixo , compre");
+                        contador = 0;
+                    }
+                    
+                }
+                await Task.Delay(timer);
+
+            }
+            
+            
+            
         }
         else
         {
